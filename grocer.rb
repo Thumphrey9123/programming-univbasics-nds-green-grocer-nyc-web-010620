c676b8fe0e726600c26caa6707d4124579f2eab1
Thomas Humphrey <thumphrey9123@gmail.com>
2019-12-30 17:14:50 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,7 +1,9 @@
 def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
+ if collection[name] 
+   return colection[name]
+ else
+   return nil
+ end
 end
 
 def consolidate_cart(cart)
